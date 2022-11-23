5228bd0b46facb4e89aef1123924623d4d8b3f7a
Margaux Voiselle <margaux@MacBook-Air-de-Margaux.local>
2022-11-23 19:43:57 +0100

    full day11 working

--- 2020/JavaScript/day11.js
+++ 2020/JavaScript/day11.js
@@ -1,4 +1,3 @@
-import { count } from 'console';
 import * as fs from 'fs';
 
 const timer = (script, input) => {
@@ -8,54 +7,171 @@ const timer = (script, input) => {
     return (end - start).toFixed(2);
 };
 
-const findAdjacentOccupied = (input, i, j) => {
+const countOccupiedSeat = (input) => {
+    return input.flat(1).filter((x) => x === '#').length;
+};
+
+const seatVisibleIsOccupied = (input, direction, i, j, isAdjacent) => {
+    switch (direction) {
+        case 'north':
+            i = i - 1;
+            if (isAdjacent) {
+                if (input[i][j] === '#') return true;
+                else return false;
+            }
+            while (input[i][j] === '.' && 0 < i && i <= input.length - 1 && 0 <= j && j <= input[0].length - 1) {
+                i = i - 1;
+            }
+            if (input[i][j] === '#') return true;
+            else return false;
+        case 'south':
+            i = i + 1;
+            if (isAdjacent) {
+                if (input[i][j] === '#') return true;
+                else return false;
+            }
+            while (input[i][j] === '.' && 0 <= i && i < input.length - 1 && 0 <= j && j <= input[0].length - 1) {
+                i = i + 1;
+            }
+            if (input[i][j] === '#') return true;
+            else return false;
+        case 'east':
+            j = j + 1;
+            if (isAdjacent) {
+                if (input[i][j] === '#') return true;
+                else return false;
+            }
+            while (input[i][j] === '.' && 0 <= i && i <= input.length - 1 && 0 <= j && j < input[0].length - 1) {
+                j = j + 1;
+            }
+            if (input[i][j] === '#') return true;
+            else return false;
+        case 'west':
+            j = j - 1;
+            if (isAdjacent) {
+                if (input[i][j] === '#') return true;
+                else return false;
+            }
+            while (input[i][j] === '.' && 0 <= i && i <= input.length - 1 && 0 < j && j <= input[0].length - 1) {
+                j = j - 1;
+            }
+            if (input[i][j] === '#') return true;
+            else return false;
+        case 'northWest':
+            i = i - 1;
+            j = j - 1;
+            if (isAdjacent) {
+                if (input[i][j] === '#') return true;
+                else return false;
+            }
+            while (input[i][j] === '.' && 0 < i && i <= input.length - 1 && 0 < j && j <= input[0].length - 1) {
+                i = i - 1;
+                j = j - 1;
+            }
+            if (input[i][j] === '#') return true;
+            else return false;
+        case 'northEast':
+            i = i - 1;
+            j = j + 1;
+            if (isAdjacent) {
+                if (input[i][j] === '#') return true;
+                else return false;
+            }
+            while (input[i][j] === '.' && 0 < i && i <= input.length - 1 && 0 <= j && j < input[0].length - 1) {
+                i = i - 1;
+                j = j + 1;
+            }
+            if (input[i][j] === '#') return true;
+            else return false;
+        case 'southWest':
+            i = i + 1;
+            j = j - 1;
+            if (isAdjacent) {
+                if (input[i][j] === '#') return true;
+                else return false;
+            }
+            while (input[i][j] === '.' && 0 <= i && i < input.length - 1 && 0 < j && j <= input[0].length - 1) {
+                i = i + 1;
+                j = j - 1;
+            }
+            if (input[i][j] === '#') return true;
+            else return false;
+        case 'southEast':
+            i = i + 1;
+            j = j + 1;
+            if (isAdjacent) {
+                if (input[i][j] === '#') return true;
+                else return false;
+            }
+            while (input[i][j] === '.' && 0 <= i && i < input.length - 1 && 0 <= j && j < input[0].length - 1) {
+                i = i + 1;
+                j = j + 1;
+            }
+            if (input[i][j] === '#') return true;
+            else return false;
+    }
+};
+
+const findOccupied = (input, i, j, isAdjacent) => {
     let result = 0;
     if (i === 0) {
         if (j === 0) {
-            if (input[i][j + 1] === '#') result++;
-            if (input[i + 1][j] === '#') result++;
-            if (input[i + 1][j + 1] === '#') result++;
+            if (seatVisibleIsOccupied(input, 'east', i, j, isAdjacent)) result++;
+            if (seatVisibleIsOccupied(input, 'south', i, j, isAdjacent)) result++;
+            if (seatVisibleIsOccupied(input, 'southEast', i, j, isAdjacent)) result++;
         } else if (j === input[0].length - 1) {
-            if (input[i][j - 1] === '#') result++;
-            if (input[i + 1][j - 1] === '#') result++;
-            if (input[i + 1][j] === '#') result++;
+            if (seatVisibleIsOccupied(input, 'west', i, j, isAdjacent)) result++;
+            if (seatVisibleIsOccupied(input, 'south', i, j, isAdjacent)) result++;
+            if (seatVisibleIsOccupied(input, 'southWest', i, j, isAdjacent)) result++;
         } else {
-            if (input[i][j - 1] === '#') result++;
-            if (input[i][j + 1] === '#') result++;
-            if (input[i + 1][j - 1] === '#') result++;
-            if (input[i + 1][j] === '#') result++;
-            if (input[i + 1][j + 1] === '#') result++;
+            if (seatVisibleIsOccupied(input, 'west', i, j, isAdjacent)) result++;
+            if (seatVisibleIsOccupied(input, 'east', i, j, isAdjacent)) result++;
+            if (seatVisibleIsOccupied(input, 'south', i, j, isAdjacent)) result++;
+            if (seatVisibleIsOccupied(input, 'southEast', i, j, isAdjacent)) result++;
+            if (seatVisibleIsOccupied(input, 'southWest', i, j, isAdjacent)) result++;
         }
     } else if (i === input.length - 1) {
         if (j === 0) {
-            if (input[i - 1][j] === '#') result++;
-            if (input[i - 1][j + 1] === '#') result++;
-            if (input[i][j + 1] === '#') result++;
+            if (seatVisibleIsOccupied(input, 'north', i, j, isAdjacent)) result++;
+            if (seatVisibleIsOccupied(input, 'east', i, j, isAdjacent)) result++;
+            if (seatVisibleIsOccupied(input, 'northEast', i, j, isAdjacent)) result++;
         } else if (j === input[0].length - 1) {
-            if (input[i - 1][j - 1] === '#') result++;
-            if (input[i - 1][j] === '#') result++;
-            if (input[i][j - 1] === '#') result++;
+            if (seatVisibleIsOccupied(input, 'north', i, j, isAdjacent)) result++;
+            if (seatVisibleIsOccupied(input, 'west', i, j, isAdjacent)) result++;
+            if (seatVisibleIsOccupied(input, 'northWest', i, j, isAdjacent)) result++;
         } else {
-            if (input[i - 1][j - 1] === '#') result++;
-            if (input[i - 1][j] === '#') result++;
-            if (input[i - 1][j + 1] === '#') result++;
-            if (input[i][j - 1] === '#') result++;
-            if (input[i][j + 1] === '#') result++;
+            if (seatVisibleIsOccupied(input, 'west', i, j, isAdjacent)) result++;
+            if (seatVisibleIsOccupied(input, 'east', i, j, isAdjacent)) result++;
+            if (seatVisibleIsOccupied(input, 'north', i, j, isAdjacent)) result++;
+            if (seatVisibleIsOccupied(input, 'northEast', i, j, isAdjacent)) result++;
+            if (seatVisibleIsOccupied(input, 'northWest', i, j, isAdjacent)) result++;
         }
+    } else if (j === 0 && i > 0) {
+        if (seatVisibleIsOccupied(input, 'north', i, j, isAdjacent)) result++;
+        if (seatVisibleIsOccupied(input, 'east', i, j, isAdjacent)) result++;
+        if (seatVisibleIsOccupied(input, 'south', i, j, isAdjacent)) result++;
+        if (seatVisibleIsOccupied(input, 'northEast', i, j, isAdjacent)) result++;
+        if (seatVisibleIsOccupied(input, 'southEast', i, j, isAdjacent)) result++;
+    } else if (j === input[0].length - 1 && i > 0) {
+        if (seatVisibleIsOccupied(input, 'north', i, j, isAdjacent)) result++;
+        if (seatVisibleIsOccupied(input, 'west', i, j, isAdjacent)) result++;
+        if (seatVisibleIsOccupied(input, 'south', i, j, isAdjacent)) result++;
+        if (seatVisibleIsOccupied(input, 'northWest', i, j, isAdjacent)) result++;
+        if (seatVisibleIsOccupied(input, 'southWest', i, j, isAdjacent)) result++;
     } else {
-        if (input[i - 1][j - 1] === '#') result++;
-        if (input[i - 1][j] === '#') result++;
-        if (input[i - 1][j + 1] === '#') result++;
-        if (input[i][j - 1] === '#') result++;
-        if (input[i][j + 1] === '#') result++;
-        if (input[i + 1][j - 1] === '#') result++;
-        if (input[i + 1][j] === '#') result++;
-        if (input[i + 1][j + 1] === '#') result++;
+        if (seatVisibleIsOccupied(input, 'west', i, j, isAdjacent)) result++;
+        if (seatVisibleIsOccupied(input, 'east', i, j, isAdjacent)) result++;
+        if (seatVisibleIsOccupied(input, 'north', i, j, isAdjacent)) result++;
+        if (seatVisibleIsOccupied(input, 'northEast', i, j, isAdjacent)) result++;
+        if (seatVisibleIsOccupied(input, 'northWest', i, j, isAdjacent)) result++;
+        if (seatVisibleIsOccupied(input, 'south', i, j, isAdjacent)) result++;
+        if (seatVisibleIsOccupied(input, 'southEast', i, j, isAdjacent)) result++;
+        if (seatVisibleIsOccupied(input, 'southWest', i, j, isAdjacent)) result++;
     }
     return result;
 };
 
-const updateSeat = (input) => {
+const updateSeat = (input, isAdjacent) => {
     const inputUpdated = [];
     for (let list of input) {
         let listUpdated = [];
@@ -64,25 +180,33 @@ const updateSeat = (input) => {
         }
         inputUpdated.push(listUpdated);
     }
+    let min = 5;
+    if (isAdjacent) min = 4;
     for (let i = 0; i < input.length; i++) {
         for (let j = 0; j < input.length; j++) {
-            if (findAdjacentOccupied(input, i, j) === 0 && input[i][j] === 'L') inputUpdated[i][j] = '#';
-            else if (findAdjacentOccupied(input, i, j) >= 4 && input[i][j] === '#') inputUpdated[i][j] = 'L';
+            if (input[i][j] == '.') inputUpdated[i][j] = '.';
+            if (findOccupied(input, i, j, isAdjacent) === 0 && input[i][j] === 'L') inputUpdated[i][j] = '#';
+            else if (findOccupied(input, i, j, isAdjacent) >= min && input[i][j] === '#') inputUpdated[i][j] = 'L';
             else inputUpdated[i][j] = input[i][j];
         }
     }
     return inputUpdated;
 };
 
-const countOccupiedSeat = (input) => {
-    return input.flat(1).filter((x) => x === '#').length;
-};
-
 const partOne = (input) => {
-    let inputUpdated = updateSeat(input);
+    let inputUpdated = updateSeat(input, true);
     while (input.toString() !== inputUpdated.toString()) {
         input = inputUpdated;
-        inputUpdated = updateSeat(input);
+        inputUpdated = updateSeat(input, true);
+    }
+    return countOccupiedSeat(input);
+};
+
+const partTwo = (input) => {
+    let inputUpdated = updateSeat(input, false);
+    while (input.toString() !== inputUpdated.toString()) {
+        input = inputUpdated;
+        inputUpdated = updateSeat(input, false);
     }
     return countOccupiedSeat(input);
 };
@@ -94,5 +218,5 @@ const partOne = (input) => {
         .split('\n')
         .map((list) => list.split(''));
     console.log(`Result of part one for ${file} : ` + partOne(input) + ` (executed in ${timer(partOne, input)} ms)`);
-    // console.log(`Result of part two for ${file} : ` + partTwo(input) + ` (executed in ${timer(partTwo, input)} ms)`);
+    console.log(`Result of part two for ${file} : ` + partTwo(input) + ` (executed in ${timer(partTwo, input)} ms)`);
 });
